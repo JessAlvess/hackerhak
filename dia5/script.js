18ba974000584914e8fa96ad887080e4e7f74156
Jessé Alves <jessealvespr@outlook.com>
2024-02-14 19:39:46 -0300

    desafio dia5 terminado

--- dia5/script.js
+++ dia5/script.js
@@ -9,14 +9,14 @@ const input6 = 'S;V;pictureFrame'
 
 function processData(input) {
     //Enter your code here
-let method = input[0]
-let type = input[2]
-let inputText = input.slice(4)
-let upperCaseIndex = []
-const isUpperCase = str => str === str.toUpperCase()
-let outputText = ''
+    let method = input[0]
+    let type = input[2]
+    let inputText = input.slice(4)
+    let upperCaseIndex = []
+    const isUpperCase = str => str === str.toUpperCase()
+    let outputText = ''
 
-function splitMethod(inputText) {
+    function splitMethod(inputText) {
     for (let index = 0; index < inputText.length - 2; index++) {
         if (isUpperCase(inputText[index])) {
             upperCaseIndex.push(index)
@@ -36,14 +36,12 @@ function splitMethod(inputText) {
         wordsArr.push(' ')
         lastIndex = [upperCaseIndex[index]]
     }
-    let finalText = ''
-    for (let index = 0; index < wordsArr.length; index++) {
-        finalText = ''.concat(...wordsArr);
+    let finalText = ''.concat(...wordsArr).trim();
+    
+    return finalText;
     }
-    return console.log(finalText);
-}
 
-function splitClass(inputText) {
+    function splitClass(inputText) {
     for (let index = 0; index < inputText.length; index++) {
         if (isUpperCase(inputText[index])) {
             upperCaseIndex.push(index)
@@ -67,23 +65,166 @@ function splitClass(inputText) {
     for (let index = 0; index < wordsArr.length; index++) {
         finalText = ''.concat(...wordsArr);
     }
-    return console.log(finalText);
-}
-
-if (method === 'S') {
-    
-    if (type === 'M') {
-        splitMethod(inputText)
-    } else if (type === 'C') {
-        splitClass(inputText)
+    return finalText;
     }
 
+    function splitVariable(inputText) {
+    for (let index = 0; index < inputText.length; index++) {
+        if (isUpperCase(inputText[index])) {
+            upperCaseIndex.push(index)
+        }        
+    }
+    outputText = inputText.slice(0, inputText.length)
+    lastIndex = 0
+    let position
+    let wordsArr = []
+    for (let index = 0 ; index <= upperCaseIndex.length; index++) {
+        if (index === upperCaseIndex.length) {
+            position = outputText.length
+        } else {
+            position = upperCaseIndex[index]
+        }
+        wordsArr.push((outputText.slice(lastIndex, position)).toLowerCase())
+        wordsArr.push(' ')
+        lastIndex = upperCaseIndex[index]
+    }
+    let finalText
+    for (let index = 0; index < wordsArr.length; index++) {
+        finalText = ''.concat(...wordsArr);
+    }
+    return finalText;
+    }
 
+    function combineVariable(inputText) {
+    for (let index = 0; index < inputText.length; index++) {
+        if (isUpperCase(inputText[index])) {
+            upperCaseIndex.push(index)
+        }        
+    }
+    outputText = inputText.slice(0, inputText.length)
+    lastIndex = 0
+    let position
+    let wordsArr = []
+    let finalText
+    for (let index = 0 ; index <= upperCaseIndex.length; index++) {
+        if (index === upperCaseIndex.length) {
+            position = outputText.length
+        } else {
+            position = upperCaseIndex[index]
+        }
+        if (index === 0) {
+            wordsArr.push(((outputText.slice(lastIndex, position)).toLowerCase()).trim())
+            
+        } else {
+            let temporaryString = ((outputText.slice(lastIndex, position)).toLowerCase()).trim()
+            let upperCaseFirstLetter = temporaryString.charAt(0).toUpperCase()
+            let temporaryStringSlices = temporaryString.slice(1)
+            let finalString = ''.concat(upperCaseFirstLetter, temporaryStringSlices)
+            wordsArr.push(finalString)
+        }
+        lastIndex = upperCaseIndex[index]
+    }
 
-} else {
+    for (let index = 0; index < wordsArr.length; index++) {
+        finalText = ''.concat(...wordsArr);
+    }
+    return finalText;
+    }
+
+    function combineMethod(inputText) {
+    for (let index = 0; index < inputText.length; index++) {
+        if (isUpperCase(inputText[index])) {
+            upperCaseIndex.push(index)
+        }        
+    }
+    outputText = inputText.slice(0, inputText.length)
+    lastIndex = 0
+    let position
+    let wordsArr = []
+    let finalText
+    for (let index = 0 ; index <= upperCaseIndex.length; index++) {
+        if (index === upperCaseIndex.length) {
+            position = outputText.length
+        } else {
+            position = upperCaseIndex[index]
+        }
+        if (index === 0) {
+            wordsArr.push(((outputText.slice(lastIndex, position)).toLowerCase()).trim())
+            
+        } else {
+            let temporaryString = ((outputText.slice(lastIndex, position)).toLowerCase()).trim()
+            let upperCaseFirstLetter = temporaryString.charAt(0).toUpperCase()
+            let temporaryStringSlices = temporaryString.slice(1)
+            let finalString = ''.concat(upperCaseFirstLetter, temporaryStringSlices)
+            wordsArr.push(finalString)
+        }
+        lastIndex = upperCaseIndex[index]
+    }
+    wordsArr.push('()')
     
-}
+    for (let index = 0; index < wordsArr.length; index++) {
+        finalText = ''.concat(...wordsArr);
+    }
+    return finalText;
+    }
 
+    function combineClass(inputText) {
+    for (let index = 0; index < inputText.length; index++) {
+        if (isUpperCase(inputText[index])) {
+            upperCaseIndex.push(index)
+        }        
+    }
+    outputText = inputText.slice(0, inputText.length)
+    lastIndex = 0
+    let position
+    let wordsArr = []
+    let finalText
+    for (let index = 0 ; index <= upperCaseIndex.length; index++) {
+        if (index === upperCaseIndex.length) {
+            position = outputText.length
+        } else {
+            position = upperCaseIndex[index]
+        }
+        let temporaryString = ((outputText.slice(lastIndex, position)).toLowerCase()).trim()
+        let upperCaseFirstLetter = temporaryString.charAt(0).toUpperCase()
+        let temporaryStringSlices = temporaryString.slice(1)
+        let finalString = ''.concat(upperCaseFirstLetter, temporaryStringSlices)
+        wordsArr.push(finalString)
+        
+        lastIndex = upperCaseIndex[index]
+    }
+
+    for (let index = 0; index < wordsArr.length; index++) {
+        finalText = ''.concat(...wordsArr);
+    }
+    return finalText;
+    }
+
+    if (method === 'S') {
+    if (type === 'M') {
+        return splitMethod(inputText)
+    } else if (type === 'C') {
+        return splitClass(inputText)
+    } else {
+        return splitVariable(inputText)
+    }
+    } else {
+    if (type === 'V') {
+        return combineVariable(inputText)
+    } else if (type === 'M') {
+        return combineMethod(inputText)
+    } else {
+        return combineClass(inputText)
+    }
+    }
 } 
 
+console.log(processData(input));
+console.log(processData(input1));
+console.log(processData(input2));
 console.log(processData(input3));
+console.log(processData(input4));
+console.log(processData(input5));
+console.log(processData(input6));
+
+
