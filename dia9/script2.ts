0c17bd90005b07014a6eabb22dcd29245a278904
Jessé Alves <jessealvespr@outlook.com>
2024-03-14 19:41:37 -0300

    feat: week2 dia1

--- dia9/script2.ts
+++ dia9/script2.ts
@@ -1,4 +1,15 @@
-var input = [1, 2, 3, 4, 3, 2, 1];
-function lonelyinteger(a) {
+var input = [1, 2, 3, 4, 3, 2, 1,4];
+
+function lonelyinteger(a: number[]): number {
     // Write your code here
+    const lonelyNumber: number[] = a.filter((element) => {
+        let count: number = 0
+        for (let elementInArray of a) {
+            count += element === elementInArray ? 1 : 0
+        }
+        return count === 1 ? element : null
+    })
+    return lonelyNumber[0]
 }
+
+console.log(lonelyinteger(input))
